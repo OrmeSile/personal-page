47f775a708af1de579e907cf895d246564d88ee8
Vivien L'Helguen <95030000+OrmeSile@users.noreply.github.com>
2023-12-20 14:27:59 +0100

    fixed config for build

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 const {PHASE_EXPORT} = require("next/constants");
 
-module.exports = (phase) => {
-  const outputConfig = phase === PHASE_EXPORT ? {
-    output: 'export'
-  } : {}
-  return {
-    ...outputConfig,
-    webpack: (config) => {
-      config.module.rules.push({
-          test: /\.svg$/i,
-          use: ['@svgr/webpack'],
-        },
-      )
-      return config
+module.exports = (phase, {defaultConfig}) => {
+  const webpackConfig = (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  }
+
+  if (phase === PHASE_EXPORT) {
+    return {
+      output: 'export',
+      webpack: webpackConfig
     }
   }
+  return {webpack: webpackConfig}
 }
